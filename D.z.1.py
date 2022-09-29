44875af23cac05a18c88dd9ea44b79577fbbebea
Anatolii76V <110255317+Anatolii76V@users.noreply.github.com>
2022-09-29 13:49:00 +0300

    Update D.z.1.py

--- D.z.1.py
+++ D.z.1.py
@@ -41,35 +41,7 @@ if x > 0 and y < 0:
     print('4')
       
       
-   ''' задача 4 HARD необязательная Напишите простой калькулятор, который считывает с пользовательского ввода
-    три строки: первое число, второе число и операцию, после чего применяет операцию к введённым числам 
-    ("первое число" "операция" "второе число") и выводит результат на экран.'''
-    
-    
-A = float (input())
-B = float (input())
-C = str (input())
-if C =='+':
-    print(A+B)
-elif C=='-':
-    print(A-B)
-elif C=='*':
-    print(A*B)
-elif C=='/' and B==0:
-    print("Деление на 0!")
-elif C=='/' and B!=0:
-    print(A/B)
-elif C=='mod' and B==0:
-    print('Деление на 0!')
-elif C=='mod' and B!=0:
-    print(A%B)
-elif C=='pow':
-    print(A**B)
-elif C=='div' and B==0:
-    print('Деление на 0!')
-elif C=='div' and B!=0:
-    print(A//B)
-    
+
    
    
       
